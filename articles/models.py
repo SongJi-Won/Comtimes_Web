6cb1cebad3d737708b42f0966e52d717b3b9d033
SongJi-Won <s26788761@naver.com>
2019-07-29 01:08:43 +0900

    Comtimes_2 +글 뒤에 이미지 추가해서 보여주기 +기사리스트에서 100글자 & 상세페이지

--- articles/models.py
+++ articles/models.py
@@ -23,18 +23,21 @@ class Post(models.Model):
     def __str__(self):
         return self.title
 
+    def summary(self):
+        return self.content[:100]
 
-# def article_img_path(instance, filename):
-#     extension = filename.split('.')[-1]
-#     # return 'article/{}/{}.{}'.format(instance.post.id, instance.id, extension)
-#     return 'article/%s/%s/%s' % (instance.post.id, instance.id, extension)
-#
-# class Image(models.Model):
-#     post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='post_images')
-#     image = models.ImageField(upload_to=article_img_path)
-#     explanation = models.CharField(max_length=100)
-#
-#     def __str__(self):
-#         return article_img_path(self, self.for)
+
+def article_img_path(instance, filename):
+    extension = filename.split('.')[-1]
+    # return 'article/{}/{}.{}'.format(instance.post.id, instance.id, extension)
+    return 'article_image/%s/%s/%s' % (instance.post.id, instance.id, extension)
+
+class Image(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='post_images')
+    image = models.ImageField(upload_to=article_img_path)
+    explanation = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.explanation
 
 
--- articles/views.py
+++ articles/views.py
@@ -1,7 +1,15 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Post
 
 # Create your views here.
 def home(request):
     posts = Post.objects
-    return render(request, 'home.html', {'posts': posts})
+    return render(request, 'home.html', {'posts': posts, })
+
+def detail(request, article_id):
+    post_detail = get_object_or_404(Post, pk=article_id)
+    images = post_detail.post_images
+
+
+    return render(request, 'detail.html', {'post': post_detail, 'images':images})
+
