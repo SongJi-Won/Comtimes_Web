e65ef8b1f0c22f978f51c6c13fce476b3b678eaa
SongJi-Won <s26788761@naver.com>
2019-07-28 23:57:32 +0900

    Comtimes_1 기본적인 글만 리스트로 보여주는 상태

---
+++ articles/admin.py
@@ -0,0 +1,9 @@
+from django.contrib import admin
+# from .models import User, Post
+# from .models import Post, Image
+from .models import Post
+
+
+# admin.site.register(User)
+admin.site.register(Post)
+# admin.site.register(Image)
---
+++ articles/models.py
@@ -0,0 +1,40 @@
+from django.db import models
+
+# Create your models here.
+
+# class User(models.Model):
+#     user_number = models.IntegerField(primary_key=True)
+#     user_id = models.CharField(max_length=20)
+#     password = models.CharField(max_length=20)
+#     name = models.CharField(max_length=10)
+#     email = models.CharField(max_length=20)
+#     is_reporter = models.BooleanField(default= False)
+#
+#     def __str__(self):
+#         return self.name
+
+
+class Post(models.Model):
+    title = models.CharField(max_length=100)
+    sub_title = models.CharField(max_length=100)
+    # author = models.ForeignKey('User', on_delete=models.CASCADE)
+    content = models.TextField()
+
+    def __str__(self):
+        return self.title
+
+
+# def article_img_path(instance, filename):
+#     extension = filename.split('.')[-1]
+#     # return 'article/{}/{}.{}'.format(instance.post.id, instance.id, extension)
+#     return 'article/%s/%s/%s' % (instance.post.id, instance.id, extension)
+#
+# class Image(models.Model):
+#     post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='post_images')
+#     image = models.ImageField(upload_to=article_img_path)
+#     explanation = models.CharField(max_length=100)
+#
+#     def __str__(self):
+#         return article_img_path(self, self.for)
+
+
